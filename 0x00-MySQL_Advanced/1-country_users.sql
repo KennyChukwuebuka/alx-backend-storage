850884367ad6330e39ed7ba520452496e2648d99
kennyChukwuebuka <kenkinado@gmail>
2024-04-25 11:22:30 +0100

    added task

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Create a table named `users` with the following columns:
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
