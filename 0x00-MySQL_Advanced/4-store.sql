1ad53569b724f29a1a463a0aebe346555aecf3fb
kennyChukwuebuka <kenkinado@gmail>
2024-04-25 18:34:03 +0100

    task 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,21 @@
+DELIMITER //
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    DECLARE item_qty INT;
+    
+    -- Get the current quantity of the item
+    SELECT quantity INTO item_qty
+    FROM items
+    WHERE name = NEW.item_name;
+    
+    -- Update the quantity of the item in the items table
+    UPDATE items
+    SET quantity = GREATEST(item_qty - NEW.number, 0)
+    WHERE name = NEW.item_name;
+END;
+//
+
+DELIMITER ;
