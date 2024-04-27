71668601af6d2be0fa1234b8ba2a20d56089fb33
kennyChukwuebuka <kenkinado@gmail>
2024-04-27 23:04:41 +0100

    adv

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,31 +1,15 @@
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
-    IN user_id INT
-)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_weight FLOAT;
-    DECLARE avg_score FLOAT;
-
-    -- Calculate total weighted score for the user
-    SELECT SUM(corrections.score * projects.weight) INTO total_score
-    FROM corrections
-    INNER JOIN projects ON corrections.project_id = projects.id
-    WHERE corrections.user_id = user_id;
-
-    -- Calculate total weight for the user's projects
-    SELECT SUM(weight) INTO total_weight
-    FROM projects;
-
-    -- Calculate average weighted score
-    SET avg_score = total_score / total_weight;
-
-    -- Update the user's average_score in the users table
     UPDATE users
-    SET average_score = avg_score
+    SET average_score = (
+        SELECT SUM(c.score * p.weight) / SUM(p.weight)
+        FROM corrections c
+        JOIN projects p ON c.project_id = p.id
+        WHERE c.user_id = user_id
+    )
     WHERE id = user_id;
-
 END //
 
 DELIMITER ;
