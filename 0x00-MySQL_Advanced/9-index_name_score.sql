94a7f330228c29dd74f127887ce0966b955447fb
kennyChukwuebuka <kenkinado@gmail>
2024-04-27 15:54:16 +0100

    task

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,7 +1,2 @@
--- Assuming the table 'names' already exists
-
--- Drop the index if it already exists
-DROP INDEX IF EXISTS idx_name_first_score ON names;
-
--- Create the index on the first letter of the name column and the score column
-CREATE INDEX idx_name_first_score ON names (name(1), score);
+-- Write a SQL script that creates an index idx_name_first_score on the table names and the first letter of name and the score.
+CREATE INDEX idx_name_first_score ON names(name(1), score)
