44c97404d608622b0997a4b680d0ac88bb5259f0
kennyChukwuebuka <kenkinado@gmail>
2024-04-30 21:43:28 +0100

    added adv

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,45 +1,39 @@
 #!/usr/bin/env python3
-"""Improve
+"""Improve your previous log_stats.py script by adding
 """
 
+import pymongo
 
-from pymongo import MongoClient
+# Connect to MongoDB
+client = pymongo.MongoClient()
+db = client['logs']
+collection = db['nginx']
 
+# Get the total number of documents
+total_docs = collection.count_documents({})
 
-def get_log_stats():
-    """Get stats about Nginx logs"""
-    # Connect to MongoDB
-    client = MongoClient()
-    db = client.logs
-    collection = db.nginx
+# Get the count of documents by method
+methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+method_counts = [collection.count_documents({'method':
+                                             method}) for method in methods]
 
-    # Total number of documents
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+# Get the count of documents with method=GET and path=/status
+status_checks = collection.count_documents({'method': 'GET',
+                                            'path': '/status'})
 
-    # Count methods
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("Methods:")
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"    method {method}: {count}")
+# Get the top 10 IPs
+ips = collection.aggregate([
+    {'$group': {'_id': '$ip', 'count': {'$sum': 1}}},
+    {'$sort': {'count': -1}},
+    {'$limit': 10}
+])
 
-    # Count status check
-    count_status_check = collection.count_documents({"method": "GET",
-                                                     "path": "/status"})
-    print(f"{count_status_check} status check")
-
-    # Top 10 IPs
-    print("IPs:")
-    pipeline = [
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ]
-    top_ips = collection.aggregate(pipeline)
-    for index, ip in enumerate(top_ips, start=1):
-        print(f"    {ip['_id']}: {ip['count']}")
-
-
-if __name__ == "__main__":
-    get_log_stats()
+# Print the results
+print(f'{total_docs} logs')
+print('Methods:')
+for method, count in zip(methods, method_counts):
+    print(f'\tmethod {method}: {count}')
+print(f'{status_checks} status check')
+print('IPs:')
+for ip in ips:
+    print(f'{ip["_id"]}: {ip["count"]}')
