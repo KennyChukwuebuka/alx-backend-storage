8a1151ba755fd7e71632fc08da4fa8350e486f97
kennyChukwuebuka <kenkinado@gmail>
2024-05-01 21:28:43 +0100

    added

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,11 +10,11 @@ from functools import wraps
 
 
 def call_history(method: Callable) -> Callable:
-    """function call_history
+    """function that returns the history of
     """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """function wrapper
+        """function that returns the history of
         """
         inputs_key = f"{method.__qualname__}:inputs"
         outputs_key = f"{method.__qualname__}:outputs"
@@ -33,16 +33,27 @@ def call_history(method: Callable) -> Callable:
 
 
 class Cache:
+
     def __init__(self):
-        """function __init__
+        """function that returns the history of
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @call_history
     def store(self, data):
-        """function store
-        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def replay(self, method: Callable):
+        inputs_key = f"{method.__qualname__}:inputs"
+        outputs_key = f"{method.__qualname__}:outputs"
+
+        inputs = self._redis.lrange(inputs_key, 0, -1)
+        outputs = self._redis.lrange(outputs_key, 0, -1)
+
+        print(f"{method.__qualname__} was called {len(inputs)} times:")
+        for input_args, output in zip(inputs, outputs):
+            input_args = eval(input_args.decode())
+            print(f"{method.__qualname__}(*{input_args}) -> {output.decode()}")
